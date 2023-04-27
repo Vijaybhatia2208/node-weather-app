4010619045c70b1d32d963cdee0051091337f23e
Vijay Bhatia <shanubhatia123@gmail.com>
2023-04-28 00:55:53 +0530

    init command

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -14,15 +11,16 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:4000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
+                console.log(data.error)
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
+                console.log(data.location)
             }
         })
     })
-
 })
--- src/app.js
+++ src/app.js
@@ -100,5 +100,5 @@ app.get('*', (req, res) => {
 //app.com/about
 
 app.listen(4000, () => {
-    console.log('Server is up on port 3000.')
+    console.log('Server is up on port 4000.')
 })
